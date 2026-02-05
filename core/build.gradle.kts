62018732589dafc24ade2cbbeca2b5e4d4d6531b
alexandrenavarro <alexandrenavarro@gmail.com>
2026-02-04 21:54:48 -0300

    chore: remove unused dispatcher infrastructure and AppResult extensions

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -1,8 +1,6 @@
 plugins {
     alias(libs.plugins.android.library)
     alias(libs.plugins.kotlin.android)
-    alias(libs.plugins.ksp)
-    alias(libs.plugins.hilt.android)
 }
 
 android {
@@ -35,12 +33,5 @@ android {
 }
 
 dependencies {
-    implementation(libs.kotlinx.coroutines.core)
-    implementation(libs.kotlinx.coroutines.android)
-    implementation(libs.hilt.android)
-    ksp(libs.hilt.compiler)
-
     testImplementation(libs.junit)
-    testImplementation(libs.kotlinx.coroutines.test)
-    testImplementation(libs.hilt.android.testing)
 }
--- core/src/main/java/com/alexandrenavarro/topheadlines/core/result/AppResult.kt
+++ core/src/main/java/com/alexandrenavarro/topheadlines/core/result/AppResult.kt
@@ -9,23 +9,3 @@ inline fun <T, R> AppResult<T>.map(transform: (T) -> R): AppResult<R> = when (th
     is AppResult.Success -> AppResult.Success(transform(data))
     is AppResult.Error -> this
 }
-
-inline fun <T> AppResult<T>.onSuccess(action: (T) -> Unit): AppResult<T> {
-    if (this is AppResult.Success) action(data)
-    return this
-}
-
-inline fun <T> AppResult<T>.onError(action: (Throwable) -> Unit): AppResult<T> {
-    if (this is AppResult.Error) action(exception)
-    return this
-}
-
-fun <T> AppResult<T>.getOrNull(): T? = when (this) {
-    is AppResult.Success -> data
-    is AppResult.Error -> null
-}
-
-fun <T> AppResult<T>.getOrThrow(): T = when (this) {
-    is AppResult.Success -> data
-    is AppResult.Error -> throw exception
-}
